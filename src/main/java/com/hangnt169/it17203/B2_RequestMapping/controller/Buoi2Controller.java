debc3ffe04dce0c991f8816775b4bdf33c6cd2a7
HangNT169 <HangNT169@fpt.edu.vn>
2023-01-12 13:26:32 +0700

    Buổi 5: Request Mapping

--- src/main/java/com/hangnt169/it17203/B2_RequestMapping/controller/Buoi2Controller.java
+++ src/main/java/com/hangnt169/it17203/B2_RequestMapping/controller/Buoi2Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,11 +17,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/sinh-vien/")
 public class Buoi2Controller {
 
     private List<SinhVien> sinhViens = new ArrayList<>();
 
-    @GetMapping("/sinh-vien/hien-thi")
+    @GetMapping("hien-thi")
     public String hienThiDanhSachSinhVien(Model model) {
 
         if (sinhViens.isEmpty()) {
@@ -35,7 +37,7 @@ public class Buoi2Controller {
         return "buoi3/buoi2";
     }
 
-    @GetMapping("/sinh-vien/detail/{maSinhVien}")
+    @GetMapping("detail/{maSinhVien}")
     public String detailSinhVien(@PathVariable("maSinhVien") String ma, Model model) {
         for (SinhVien sv : sinhViens) {
             if (sv.getMa().equals(ma)) {
@@ -45,7 +47,7 @@ public class Buoi2Controller {
         return "buoi3/detailSinhVien";
     }
 
-    @GetMapping("/sinh-vien/delete/{maSinhVien}")
+    @GetMapping("delete/{maSinhVien}")
     public String deleteSinhVien(@PathVariable("maSinhVien") String ma) {
         for (SinhVien sv : sinhViens) {
             if (sv.getMa().equals(ma)) {
@@ -56,7 +58,7 @@ public class Buoi2Controller {
         return "redirect:/sinh-vien/hien-thi";
     }
 
-    @GetMapping("/sinh-vien/search")
+    @GetMapping("search")
     public String searchSinhVien(@RequestParam(name = "ten1", required = false, defaultValue = "") String ten, Model model) {
         List<SinhVien> list = new ArrayList<>();
         for (SinhVien sv : sinhViens) {
@@ -68,7 +70,7 @@ public class Buoi2Controller {
         return "buoi3/buoi2";
     }
 
-    @GetMapping("/sinh-vien/view-add")
+    @GetMapping("view-add")
     public String viewAdd(Model model) {
         // tao 1 contrutor khong tham so de truyen sang jsp
         model.addAttribute("sv1", new SinhVien());
@@ -82,7 +84,7 @@ public class Buoi2Controller {
 //        model.addAttribute("sinhViens", listSinhVien);
 //        return "buoi2/listSinhVien";
 //    }
-    @PostMapping("/sinh-vien/add")
+    @PostMapping("add")
     public String addStudent(@Valid @ModelAttribute("sv1") SinhVien sinhVien, BindingResult result) {
         if (result.hasErrors()) {
             return "buoi3/addStudent";
@@ -94,7 +96,7 @@ public class Buoi2Controller {
     }
 
     @ResponseBody
-    @GetMapping("/sinh-vien/all")
+    @GetMapping("all")
     public List<SinhVien> getAll() {
         sinhViens.add(new SinhVien("hangnt169", "Nguyễn Thuý Hằng", 10));
         sinhViens.add(new SinhVien("nguyenvv4", "Vũ Văn Nguyên", 101));
